a859f2d4efe83aab34beff2f28dc5f604b39df24
Akash Pal <akash.pal@techchefz.com>
2023-02-21 14:25:00 +0530

    feature/team-section is completed

--- src/Component/FeatureHeading/index.jsx
+++ src/Component/FeatureHeading/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import "./index.css";
 
-const FeatureHeading = ({ feature, heading, font, border }) => {
+const FeatureHeading = ({ feature, heading, font, margin, border }) => {
   return (
     <h4
-      className={`feature_heading feature_${border} feature__${font}  title__${heading} `}
+      className={`feature_heading feature_${margin} feature_${border} feature__${font}  title__${heading} `}
     >
       {feature}
     </h4>
---
+++ src/Component/TeamCard/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import IconButton from "../IconButton";
+
+import classes from "./index.module.css";
+
+const TeamCards = [
+  {
+    image: "Images/team1.jpg",
+    name: "jessica swift",
+    post: "Manager",
+    description: "Lorem ipsum dolor sit amet, consectet adipiscing elit.",
+  },
+  {
+    image: "Images/team2.jpg",
+    name: "ben palmer",
+    post: "Designer",
+    description: "Lorem ipsum dolor sit amet, consectet adipiscing elit.",
+  },
+  {
+    image: "Images/team3.jpg",
+    name: "adam brown",
+    post: "Developer",
+    description: "Lorem ipsum dolor sit amet, consectet adipiscing elit.",
+  },
+];
+
+const TeamCard = () => {
+  return (
+    <>
+      {TeamCards.map((card) => (
+        <div className={`${classes["card"]}`}>
+          <img
+            className={classes["card__image"]}
+            src={card.image}
+            alt="Girl sitting in office and hold cup of coffie"
+          />
+
+          <div className={classes.card__content}>
+            <h4 className={classes["card__content--title"]}>{card.name}</h4>
+            <h5 className={classes["card__content--post"]}>{card.post}</h5>
+            <p className={classes["card__content--des"]}>{card.description}</p>
+          </div>
+          <div className={classes["card__button"]}>
+            <IconButton name="view profile" />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default TeamCard;
